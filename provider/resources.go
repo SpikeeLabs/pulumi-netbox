f741e1857915454a287ac77646f7506b243f1ba9
jeanloup.monnier <jean-loup.monnier@spikeelabs.fr>
2024-03-18 17:51:39 +0100

    fix: download url

--- provider/resources.go
+++ provider/resources.go
@@ -93,8 +93,8 @@ func Provider() tfbridge.ProviderInfo {
 		LogoURL: "",
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
-		// e.g https://github.com/org/pulumi-provider-name/releases/
-		PluginDownloadURL: "",
+		// e.g https://github.com/SpikeeLabs/pulumi-netbox/releases/
+		PluginDownloadURL: "https://github.com/org/pulumi-provider-name/releases/",
 		Description:       "A Pulumi package for creating and managing Netbox resources.",
 		// category/cloud tag helps with categorizing the package in the Pulumi Registry.
 		// For all available categories, see `Keywords` in
